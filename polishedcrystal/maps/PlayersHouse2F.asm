757fbfa7a52c24d01aa7cdcbb934e33b5b78605b
LinHero <139717683+LinHero@users.noreply.github.com>
2025-02-14 16:11:53 -0600

    Fixing Typo

--- polishedcrystal/maps/PlayersHouse2F.asm
+++ polishedcrystal/maps/PlayersHouse2F.asm
@@ -6,7 +6,7 @@ PlayersHouse2F_MapScriptHeader:
 	callback MAPCALLBACK_TILES, PlayersHouse2FSetSpawn
 
 	def_warp_events
-	warp_event  7,  0, PALET_TOWN, 1
+	warp_event  7,  0, PALLET_TOWN, 1
 
 	def_coord_events
 
